6e5ad60c70b2b4db8dd0af0b9160e9e1e3cfc411
Jeff Greene <bbhmmnyc@gmail.com>
2026-07-06 02:15:40 -0400

    Fix homepage motion stats render

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -157,7 +157,7 @@ export default function HomePage() {
           </motion.div>
           </div>
           {stats && (
-  	       <motion.div>
+              <motion.div
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.7, delay: 0.35 }}
